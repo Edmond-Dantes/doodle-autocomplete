b6ae9de32a6d74996ab9682bb81f8ce1cfb1d981
Edmond <edmond.akpan@gmail.com>
2025-09-05 16:13:20 +0900

    Fix icon relative links

--- tldr-demo/src/ui/icons.tsx
+++ tldr-demo/src/ui/icons.tsx
@@ -6,22 +6,22 @@ import type { TLUiAssetUrlOverrides } from 'tldraw'
 export const assetUrls: TLUiAssetUrlOverrides = {
   icons: {
     // app branding (for later)
-    'kevin-logo': '/icons/kevin.svg',
+    'kevin-logo': './icons/kevin.svg',
 
     // tools
-    'stamp-icon': '/icons/stamp.svg',
-    'axis-icon': '/icons/axis.svg',
-    'bat-icon': '/icons/bat.svg',
-    'boat-icon': '/icons/boat.svg',
-    'car-icon': '/icons/car.svg',
-    'cat-icon': '/icons/cat.svg',
-    'dog-icon': '/icons/dog.svg',
-    'glasses-icon': '/icons/glasses.svg',
-    'house-icon': '/icons/house.svg',
-    'moon-icon': '/icons/moon.svg',
-    'sailboat-icon': '/icons/sailboat.svg',
-    'stick-icon': '/icons/stick.svg',
-    'tree-icon': '/icons/tree.svg',
+    'stamp-icon': './icons/stamp.svg',
+    'axis-icon': './icons/axis.svg',
+    'bat-icon': './icons/bat.svg',
+    'boat-icon': './icons/boat.svg',
+    'car-icon': './icons/car.svg',
+    'cat-icon': './icons/cat.svg',
+    'dog-icon': './icons/dog.svg',
+    'glasses-icon': './icons/glasses.svg',
+    'house-icon': './icons/house.svg',
+    'moon-icon': './icons/moon.svg',
+    'sailboat-icon': './icons/sailboat.svg',
+    'stick-icon': './icons/stick.svg',
+    'tree-icon': './icons/tree.svg',
     // add future icons here: 'foo-icon': '/icons/foo.svg',
 
     // used by your colour button to hide the glyph
